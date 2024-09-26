7b9ce3345ff0d8ae9d0654a789ffb693de497988
RingoStarr298 <49015029+RingoStarr298@users.noreply.github.com>
2024-09-26 21:11:30 +0000

    V.001

---
+++ Exchange.java
@@ -0,0 +1,8 @@
+public class Exchange {
+   public void rates(String currency){
+    if(currency=="USD")
+        System.out.println("The currency type is "+currency);
+    System.out.println("Out of the if statement");
+    }
+
+    }
--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 /********************************************
-*	AUTHORS:	<your names>
+*	AUTHORS:	Cj Cryan
 * COLLABORATORS: <name of peer, tutor, instructor, anyone else who helped>
-*	LAST MODIFIED:	<date of last change>
+*	LAST MODIFIED:	9/24/24
 ********************************************/
 
 /********************************************
-*	<TITLE OF PROGRAM>
+*	Currency calc
 *********************************************
 *	PROGRAM DESCRIPTION:
-*	<1-2 sentences describing overall program>
+*	Can calculate the value of currency from one country to another
 *********************************************
 *	ALGORITHM:
 *	<Pseudocode here>
@@ -16,8 +16,7 @@
 *	STATIC METHODS:
 * <list of static methods and which teammate made each>
 *********************************************/
-
-
+import java.util.Scanner;
 public class Main 
 {
   /***** CONSTANT SECTION *****/
@@ -34,6 +33,32 @@ public class Main
 
     /***** OUTPUT SECTION *****/
     
+    Scanner Base = new Scanner(System.in);
+    System.out.println("Enter currencies current type");
+
+    String BaseCurrency = Base.nextLine().toUpperCase();
+    if (BaseCurrency.length()==3) {
+      System.out.println("Currency is in "+BaseCurrency);
+    } else {
+      System.out.println("Please use the ISO-4217 to denote the currency as "+BaseCurrency+" is not a valid input");
+      System.exit(0);
+    }
+
+    Scanner ToConvert = new Scanner(System.in);
+    System.out.println("Please enter the currency you would like to convert to");
+
+    String ConvertedCurrency=ToConvert.nextLine().toUpperCase();
+
+    Exchange exchange = new Exchange();
+    exchange.rates(BaseCurrency);
   }
   /***** STATIC METHODS *****/
+  public static void covert(int c)
+  {
+
+  }
+  public static void currencytype(String ConvertedCurrency)
+  {
+
+  }
 }
