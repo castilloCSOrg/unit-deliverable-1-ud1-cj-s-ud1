62de282d92f1518d816156aa4ab7f785850a9dbf
RingoStarr298 <49015029+RingoStarr298@users.noreply.github.com>
2024-10-11 00:31:31 +0000

    Final

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 /********************************************
 *	AUTHORS:	Cj Cryan
 * COLLABORATORS: <name of peer, tutor, instructor, anyone else who helped>
-*	LAST MODIFIED:	9/24/24
+*	LAST MODIFIED:	10/10/24
 ********************************************/
 
 /********************************************
@@ -10,78 +10,93 @@
 *	PROGRAM DESCRIPTION:
 *	Can calculate the value of currency from one country to another
 *********************************************
-*	ALGORITHM:
-*	<Pseudocode here>
-*********************************************
-*	STATIC METHODS:
-* <list of static methods and which teammate made each>
+
 *********************************************/
 import java.util.Scanner;
 public class Main 
 {
   /***** CONSTANT SECTION *****/
-
-  public static void main(String[] args)
+  public static Exchange exchange = new Exchange();
+  private static String BaseCurrency="";
+  private static String ConvertedCurrency="";
+  
+   public static void main(String[] args)
   {
     /***** OUTPUT SECTION *****/
-    //Professor approved of scanner use in UD1
-    //Scanner to learn the base currency
-    Scanner Base = new Scanner(System.in);
-    System.out.println("Enter currencies current type");
-    //Saves the scanner output and uppercases it
-    String BaseCurrency = Base.nextLine().toUpperCase();
-    //Checks if the inputed currency type is a valid one and if it isn't it exits the program
-    checkvalid(BaseCurrency);
-    //Scanner to learn the currency it is to be converted to
-    Scanner ToConvert = new Scanner(System.in);
-    System.out.println("Please enter the currency you would like to convert to");
-    //Saves the scanner output and uppercases it
-    String ConvertedCurrency=ToConvert.nextLine().toUpperCase();
-    //Checks if the inputed currency type is a valid one and if it isn't it exits the program
-    checkvalid(ConvertedCurrency);
-    //Scanner to learn how much currency to be converted
-    Scanner MonetaryAmount = new Scanner(System.in);
-    System.out.println("Please enter a whole number of the amount of money to convert");
-    String Money=MonetaryAmount.nextLine();
-    try {
-      int cash=Integer.parseInt(Money);
-    } catch(Exception e){
-      System.out.println("A integer was not input, rerun the code and enter a integer");
-      System.exit(0);
+    String Continue="Y";
+    while (Continue.equals("Y")){
+      currencytype();
+      Scanner Loop=new Scanner(System.in); 
+      System.out.println("Would you like to continue converting currency? (Y/N) ");
+      Continue = Loop.nextLine().toUpperCase();
     }
-    int cash=Integer.parseInt(Money);
-    System.out.println(cash);
-    //Allowing the Exchange.java to be utilized in Main
-    Exchange exchange = new Exchange();
-    //Determining what the exchange rates are
-    double rate1=exchange.rates(BaseCurrency);
-    double rate2=exchange.rates(ConvertedCurrency);
-    //Doing the math for the exhange rates
-    //Making it into USD
-    double cash_exchanged= cash*rate1;
-    System.out.println(cash_exchanged);
+    if (! Continue.equals("N")){
+      Scanner Loop=new Scanner(System.in); 
+      System.out.println("Invalid input, please try again (Y/N) ");
+      Continue = Loop.nextLine().toUpperCase();
+    }
+    
+    
   }
   /***** STATIC METHODS *****/
   {
 
   }
-  public static void currencytype(String ConvertedCurrency)
+  public static void currencytype()
   {
-
+    //Professor approved of scanner use in UD1
+    //Scanner to learn the base currency
+    Scanner Base = new Scanner(System.in);
+    System.out.println("Enter currencies current type");
+    //Saves the scanner output and uppercases it
+    Main.BaseCurrency = Base.nextLine().toUpperCase();
+    //Checks if the inputed currency type is a valid one and if it isn't it exits the program
+    Main.BaseCurrency=checkvalid(Main.BaseCurrency);
+    //Scanner to learn the currency it is to be converted to
+    Scanner ToConvert = new Scanner(System.in);
+    System.out.println("Please enter the currency you would like to convert to");
+    //Saves the scanner output and uppercases it
+    Main.ConvertedCurrency=ToConvert.nextLine().toUpperCase();
+    //Checks if the inputed currency type is a valid one and if it isn't it exits the program
+    Main.ConvertedCurrency=checkvalid(Main.ConvertedCurrency);
+    int cash=GetMoney();
+    //Determining what the exchange rates are
+    double rate1=exchange.rates(BaseCurrency);
+    double rate2=exchange.rates(ConvertedCurrency);
+    //Doing the math for the exhange rates
+    //Making it into USD
+    double cash_exchanged= cash*(1/rate1);
+    cash_exchanged=cash_exchanged*(rate2);
+    System.out.printf("$%,d%s%s%s%s%s$%,.2f\n",cash," coverted from ",BaseCurrency," to ",ConvertedCurrency," is ",cash_exchanged);
   }
-  public static void checkvalid(String CurrencyType)
+  public static String checkvalid(String Currency)
   {
-    if (CurrencyType.length()==3) {
-      Exchange exchange = new Exchange();
-      double rate=exchange.rates(CurrencyType);
-      if (rate!=0.0){
-      }
-      else {
-        System.out.println("Not a valid currency type, try again");
-        System.exit(0);
-      }
-      } 
+    double rate=exchange.rates(Currency);
+    while (Currency.length()!=3 || rate==0.0 ){
+      System.out.println("Not a valid currency type, try again");
+      Scanner C = new Scanner(System.in);
+      System.out.println("Enter currencies current type");
+      Currency = C.nextLine().toUpperCase();
+      rate=exchange.rates(Currency);
+      System.out.println(rate);
     }
+    return Currency;
       
   }
+  public static int GetMoney(){
+    Scanner scanner = new Scanner(System.in);
+    int intValue=0;
+    do {
+      System.out.print("Please enter how much money that you would like to convert that is above 0 \n");
+      String next = scanner.next();
+      try {
+          intValue = Integer.parseInt(next);
+      } catch (NumberFormatException exp) {
+      }
+  } while (intValue <= 0);
+  
+  return intValue;
+
+  }
+}
 
